d36b39f73ca9d913e778ffd03978264a2d596e49
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2017-12-24 18:55:12 -0200

    commit

--- server.js
+++ server.js
@@ -3,6 +3,40 @@ const app = express();
 const port = process.env.port || 3000;
 const address = 'http://localhost';
 const fs = require('fs');
+const offers = __dirname + '/json/orderbook.json';
+
+app.get('/listOffers', function (req, res) {
+    fs.readFile(offers, 'utf8', function (err, data) {
+        // console.log(data);
+        res.end(data);
+    });
+});
+
+
+app.get('/amount', function (req, res) {
+    fs.readFile(offers, 'utf8', function (err, data) {
+        let offers = JSON.parse(data);
+        let asks = offers["asks"];
+        findAmount(45000.00000000, offers["bids"]).then(v => {
+            let bids = v;
+            console.log(bids);
+            res.end(JSON.stringify(bids));
+        });
+
+    });
+});
+
+let findAmount = async function (amount, bids) {
+    let result = [];
+    for (let i = 0, len = bids.length; i < len; i++) {
+        let bid = bids[i][0];
+        if (bid == amount) {
+            result.push(bid);
+        }
+    }
+    return result;
+};
+
 
 let server = app.listen(port, function () {
     console.log("RESTful API Server started on: " + address + ":" + port);
