13aa26fb58ac38b2e0b618b5ec02f7f68ff5010e
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2017-12-25 22:15:06 -0200

    parameters

--- server.js
+++ server.js
@@ -5,9 +5,21 @@ const address = 'http://localhost';
 const fs = require('fs');
 const offers = __dirname + '/json/orderbook.json';
 
-app.get('/listOffers', function (req, res) {
+//amounts = 45000,44200;
+app.get('/offer/:amounts/:sort?', function (req, res) {
     fs.readFile(offers, 'utf8', function (err, data) {
-        res.end(data);
+        let offers = JSON.parse(data);
+
+        let asks = offers["asks"];
+        let bids = offers["bids"];
+
+        let amounts = splitParameters(req.params.amounts);
+        let sortOption = req.params.sort;
+
+        bids = findAmount(amounts, bids);
+
+        let results = sortResults(bids, sortOption);
+        res.end(JSON.stringify(bids));
     });
 });
 
@@ -15,28 +27,33 @@ let Comparator = function (a, b) {
     return a[0] > b[0] ? 1 : -1;
 };
 
-app.get('/amount', function (req, res) {
-    fs.readFile(offers, 'utf8', function (err, data) {
-        let offers = JSON.parse(data);
-        let asks = offers["asks"].sort(Comparator); // use reverse for Desc sorting
-        // let bids = findAmount(45000.00000000, offers["bids"]);
-        res.end(JSON.stringify(asks));
-    });
-});
+let splitParameters = function (values) {
+    return values !== undefined ? values.split(',') : values;
+};
 
-// TODO search for more than one amount at the time
+let sortResults = function (values, sortOption) {
+    if (sortOption !== undefined) {
+        if (sortOption === 'asc')
+            return values.sort(Comparator);
+        else
+            return values.sort(Comparator).reverse();
+    }
+    return values;
+};
 
-let findAmount = function (amount, bids) {
+let findAmount = function (amounts, bids) {
     let result = [];
     for (let i = 0; i < bids.length; i++) {
         let bid = bids[i][0];
-        if (bid == amount) {
-            result.push(bid);
+        for (let j = 0; j < amounts.length; j++) {
+            let amount = amounts[j];
+            if (bid == amount) {
+                result.push(bid);
+            }
         }
     }
     return result;
 };
-
 
 let server = app.listen(port, function () {
     console.log("RESTful API Server started on: " + address + ":" + port);
