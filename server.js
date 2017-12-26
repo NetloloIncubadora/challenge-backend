672c5290a61469ac5062420c10335251f06e5887
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2017-12-25 22:48:58 -0200

    commit

--- server.js
+++ server.js
@@ -3,22 +3,23 @@ const app = express();
 const port = process.env.port || 3000;
 const address = 'http://localhost';
 const fs = require('fs');
-const offers = __dirname + '/json/orderbook.json';
+const orderBook = __dirname + '/json/orderbook.json';
 
 //amounts = 45000,44200;
 app.get('/offer/:amounts/:sort?', function (req, res) {
-    fs.readFile(offers, 'utf8', function (err, data) {
+    fs.readFile(orderBook, 'utf8', function (err, data) {
         let offers = JSON.parse(data);
 
         let asks = offers["asks"];
         let bids = offers["bids"];
 
-        let amounts = splitParameters(req.params.amounts);
+        let amounts = SplitParameters(req.params.amounts);
         let sortOption = req.params.sort;
 
-        bids = findAmount(amounts, bids);
+        bids = FindAmount(amounts, bids);
+        // asks = FindAmount(amounts, asks);
 
-        let results = sortResults(bids, sortOption);
+        let results = SortResults(bids, sortOption);
         res.end(JSON.stringify(bids));
     });
 });
@@ -27,11 +28,11 @@ let Comparator = function (a, b) {
     return a[0] > b[0] ? 1 : -1;
 };
 
-let splitParameters = function (values) {
+let SplitParameters = function (values) {
     return values !== undefined ? values.split(',') : values;
 };
 
-let sortResults = function (values, sortOption) {
+let SortResults = function (values, sortOption) {
     if (sortOption !== undefined) {
         if (sortOption === 'asc')
             return values.sort(Comparator);
@@ -40,17 +41,39 @@ let sortResults = function (values, sortOption) {
     }
     return values;
 };
+// Should be below the lowest amount or below one of them?
+let IsBelowPrice = function (amount, offer) {
+    return offer < amount;
+}
+// Should be below the lowest amount or below one of them?
+let IsBelowPrices = function (amounts, offer) {
+    let result = false;
 
-let findAmount = function (amounts, bids) {
-    let result = [];
-    for (let i = 0; i < bids.length; i++) {
-        let bid = bids[i][0];
-        for (let j = 0; j < amounts.length; j++) {
-            let amount = amounts[j];
-            if (bid == amount) {
-                result.push(bid);
-            }
+    for (let i = 0; i < amounts.length; i++) {
+        let amount = amounts[i];
+        if (offer < amount) {
+            result = true;
+            break;
         }
+    }
+
+    return result;
+}
+
+let FindAmount = function (amounts, offers) {
+    let result = [];
+    for (let i = 0; i < offers.length; i++) {
+        let offer = offers[i][0];
+        if (IsBelowPrices)
+            result.push(offer);
+        // for (let j = 0; j < amounts.length; j++) {
+        //     let amount = amounts[j];
+        //     if (IsBelowPrice(amount, offer))
+        //         result.push(offer);
+        //     // if (offer == amount) {
+        //     //     result.push(offer);
+        //     // }
+        // }
     }
     return result;
 };
