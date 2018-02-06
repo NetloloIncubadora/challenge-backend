0b1dd1894105d8f6e05bc7ad519408f07321b5c0
Arthur Moro <arthurmoro@users.noreply.github.com>
2018-02-06 17:47:18 -0200

    UPD Teste de listagem de ASKS

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const app = require('../config/app');
 const request = require('supertest')(app);
 
 describe('Bids Search', () => {
-    it('listagem bids', (done) => {
+    it('listagem bids Genérica', (done) => {
         request.get('/bids')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/ )
@@ -16,6 +16,27 @@ describe('Bids Search', () => {
     });
     it('listagem bids with sort and amount', (done) => {
         request.get('/bids?sort=asc&amount=1,1.1,1.2,1.3,1.4,1.5,2')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/ )
+            .expect(200, done);
+    });
+});
+
+describe('Asks Search', () => {
+    it('listagem Asks Genérica', (done) => {
+        request.get('/asks')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/ )
+            .expect(200, done);
+    });
+    it('listagem asks with sort', (done) => {
+        request.get('/asks?sort=asc')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/ )
+            .expect(200, done);
+    });
+    it('listagem asks with sort and amount', (done) => {
+        request.get('/asks?sort=asc&amount=1,1.1,1.2,1.3,1.4,1.5,2')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/ )
             .expect(200, done);
